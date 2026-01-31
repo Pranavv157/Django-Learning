bcd6872053820ea6615fa6a01cb66e0274381eb9
Pranavv157 <pranavsshinde157@gmail.com>
2026-01-31 22:19:00 +0530

    Learnt about DB indexing and added it

--- users/models.py
+++ users/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 class UserProfile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    is_active = models.BooleanField(default=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,db_index=True)
+    name = models.CharField(max_length=100,db_index=True)
+    email = models.EmailField(db_index=True)
+    is_active = models.BooleanField(default=True,db_index=True)
 
 
 
